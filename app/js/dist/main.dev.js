09631454655e387a3e2c8de8424b221fe73dfeb6
mudmykola <sonysmile999@gmail.com>
2020-10-30 09:12:30 +0200

    header

--- app/js/dist/main.dev.js
+++ app/js/dist/main.dev.js
@@ -3,6 +3,14 @@
 $(function () {
   $('.header__slider').slick({
     infinite: true,
-    fade: true
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+    asNavFor: '.slider-dots'
+  });
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider'
   });
 });
--- dist/gulpfile.dev.js
+++ dist/gulpfile.dev.js
@@ -35,7 +35,7 @@ gulp.task('scss', function () {
   }));
 });
 gulp.task('css', function () {
-  return gulp.src(['node_modules/normalize.css/normalize.css', 'node_modules/slick-carousel/slick/slick.css']).pipe(concat('_libs.scss')).pipe(gulp.dest('app/scss')).pipe(browserSync.reload({
+  return gulp.src(['node_modules/normalize.css/normalize.css', 'node_modules/slick-carousel/slick/slick.css', 'node_modules/animate.css/animate.css']).pipe(concat('_libs.scss')).pipe(gulp.dest('app/scss')).pipe(browserSync.reload({
     stream: true
   }));
 });
