3d383d2fd6b1d4b4bda656c83300693029542e8e
mudmykola <sonysmile999@gmail.com>
2020-11-03 20:51:24 +0200

    surf-slider

--- app/js/dist/main.dev.js
+++ app/js/dist/main.dev.js
@@ -13,4 +13,10 @@ $(function () {
     slidesToScroll: 4,
     asNavFor: '.header__slider'
   });
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">'
+  });
 });
